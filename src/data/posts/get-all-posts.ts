4a84cbe0b5f244e2a80ddaa8570f9af95833ef98
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-08-20 18:15:30 -0300

    aula455 - adicionando query em getAllPosts

--- src/data/posts/get-all-posts.ts
+++ src/data/posts/get-all-posts.ts
@@ -2,7 +2,9 @@ import { POSTS_URL } from '../../config/app-config';
 import { PostData } from '../../domain/posts/post';
 import { fetchJson } from '../../utils/fetch-json';
 
-export const getAllPosts = async (): Promise<PostData[]> => {
-  const posts = await fetchJson<PostData[]>(POSTS_URL);
+export const getAllPosts = async (query = ''): Promise<PostData[]> => {
+  // const url = `${POSTS_URL}?&${query}`; // da aula
+  const url = `${POSTS_URL}&${query}`;
+  const posts = await fetchJson<PostData[]>(url);
   return posts['data'];
 };
