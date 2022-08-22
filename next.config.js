68b9cec2f08fac884bf8096837b23228c4a01a4b
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-08-22 14:38:30 -0300

    atualizado

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // reactStrictMode: true,
-  // swcMinify: true,
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // reactStrictMode: true,
+//   // swcMinify: true,
+//   target: 'serverless',
+// };
+
+// module.exports = nextConfig;
+
+module.exports = {
+  // Target must be serverless
   target: 'serverless',
 };
-
-module.exports = nextConfig;
