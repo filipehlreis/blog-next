12053dc90988e641fb9e5cd76d2252a509995ee9
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-08-21 22:58:07 -0300

    adicionando url de producao

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -3,4 +3,4 @@ export const POSTS_URL = `${API_URL}/posts?populate=*`;
 export const POST_URL = `${API_URL}/posts?`;
 
 export const SITE_NAME = 'Filipe Reis';
-export const SITE_URL = process.env.SITE_URL || 'http://localhost:3000';
+export const SITE_URL = 'https://splendid-pasca-6f795b.netlify.app';
