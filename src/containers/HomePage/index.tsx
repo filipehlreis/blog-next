0d244822270b14096739a333a7beb0f08001a84a
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-08-23 18:57:43 -0300

    aula478 - paginacao pt3/5

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -2,17 +2,24 @@ import Head from 'next/head';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { MainContainer } from '../../components/MainContainer';
+import { Pagination } from '../../components/Pagination';
 import { PostCard } from '../../components/PostCard';
 import { SITE_NAME } from '../../config/app-config';
+import { PaginationData } from '../../domain/posts/pagination';
 import { PostData } from '../../domain/posts/post';
 import { Category, Container } from './styles';
 
 export type HomePageProps = {
   posts: PostData[];
   category?: string;
+  pagination?: PaginationData;
 };
 
-export default function HomePage({ posts, category }: HomePageProps) {
+export default function HomePage({
+  posts,
+  category,
+  pagination,
+}: HomePageProps) {
   return (
     <>
       <Head>
@@ -34,6 +41,7 @@ export default function HomePage({ posts, category }: HomePageProps) {
             />
           ))}
         </Container>
+        <Pagination {...pagination} />
       </MainContainer>
       <Footer />
     </>
